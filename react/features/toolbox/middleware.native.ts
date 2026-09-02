86753d1c614551bb07f5aa88ace974e186cb3da6
Shadab Abbas <shadababbas@Melps-MacBook-Pro-5.local>
2026-09-02 21:01:51 +0530

    toolbar hide auto after 3 seconds.

--- react/features/toolbox/middleware.native.ts
+++ react/features/toolbox/middleware.native.ts
@@ -1,11 +1,39 @@
+import { IStore } from '../app/types';
 import { OVERWRITE_CONFIG, SET_CONFIG, UPDATE_CONFIG } from '../base/config/actionTypes';
 import MiddlewareRegistry from '../base/redux/MiddlewareRegistry';
 import { I_AM_VISITOR_MODE } from '../visitors/actionTypes';
 
-import { SET_TOOLBAR_BUTTONS } from './actionTypes';
-import { setMainToolbarThresholds } from './actions.native';
+import { SET_TOOLBAR_BUTTONS, SET_TOOLBOX_VISIBLE, TOGGLE_TOOLBOX_VISIBLE } from './actionTypes';
+import { setMainToolbarThresholds, setToolboxVisible } from './actions.native';
 import { NATIVE_THRESHOLDS, NATIVE_TOOLBAR_BUTTONS } from './constants';
 import { getToolbarButtons } from './functions.native';
+
+/**
+ * How long the native toolbox and its side toolbar remain visible after being shown.
+ */
+const TOOLBOX_AUTO_HIDE_TIMEOUT_MS = 3000;
+
+let autoHideTimeout: ReturnType<typeof setTimeout> | undefined;
+
+/**
+ * Replaces the pending auto-hide timer with one matching the current visibility.
+ *
+ * @param {Object} store - The Redux store.
+ * @returns {void}
+ */
+function _syncAutoHide(store: IStore) {
+    clearTimeout(autoHideTimeout);
+    autoHideTimeout = undefined;
+
+    if (!store.getState()['features/toolbox'].visible) {
+        return;
+    }
+
+    autoHideTimeout = setTimeout(() => {
+        autoHideTimeout = undefined;
+        store.dispatch(setToolboxVisible(false));
+    }, TOOLBOX_AUTO_HIDE_TIMEOUT_MS);
+}
 
 
 /**
@@ -18,6 +46,15 @@ import { getToolbarButtons } from './functions.native';
 
 MiddlewareRegistry.register(store => next => action => {
     switch (action.type) {
+
+    case SET_TOOLBOX_VISIBLE:
+    case TOGGLE_TOOLBOX_VISIBLE: {
+        const result = next(action);
+
+        _syncAutoHide(store);
+
+        return result;
+    }
 
     case UPDATE_CONFIG:
     case OVERWRITE_CONFIG:
